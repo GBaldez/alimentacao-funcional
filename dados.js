72b0dc5b999f81f5d7b70b537e3156890a179779
Gabriel Baldez <gabrielbaldez7@gmail.com>
2024-09-07 23:07:59 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,174 @@
+let nutrients = [
+  {
+    vitamina: "Vitamina A",
+    beneficios:
+      "Essencial para a visão, pele, dentes, gengivas, cabelo, mucosas, glândulas e sistema imunológico.",
+    fontes: [
+      "Fígado",
+      "leite",
+      "gema de ovo",
+      "queijo gordo",
+      "sardinha",
+      "óleo de fígado de bacalhau",
+      "abacate",
+      "vegetais de folhas verdes escuras",
+      "vegetais e frutas amarelas",
+    ],
+  },
+  {
+    vitamina: "Vitamina D",
+    beneficios:
+      "Fundamental para a manutenção do tecido ósseo, sistema imunológico, processo de diferenciação celular e saúde gestacional.",
+    fontes: ["Exposição solar", "peixes gordos"],
+  },
+  {
+    vitamina: "Vitamina E",
+    beneficios:
+      "Poderoso antioxidante que combate os radicais livres, diminui o risco de doenças cardíacas, previne o câncer de próstata, a doença de Alzheimer e a Esclerose Lateral Amiotrófica.",
+    fontes: [
+      "Nozes",
+      "sementes",
+      "óleos vegetais",
+      "vegetais de folhas verdes",
+      "cereais enriquecidos",
+    ],
+  },
+  {
+    vitamina: "Vitamina K",
+    beneficios: "Essencial para a coagulação sanguínea e saúde óssea.",
+    fontes: [
+      "Brócolis",
+      "couve-flor",
+      "agrião",
+      "rúcula",
+      "repolho",
+      "nabo",
+      "alface",
+      "espinafre",
+      "vegetais verdes",
+      "oleaginosas",
+      "abacate",
+      "ovo",
+    ],
+  },
+  {
+    vitamina: "Vitamina C",
+    beneficios:
+      "Necessária para a produção de colágeno, participa da biossíntese e da excreção do colesterol, é antioxidante, atua no sistema imunológico, aumenta a absorção de ferro e favorece a cicatrização.",
+    fontes: ["Frutas verdes", "amarelas", "vermelhas", "folhas verdes"],
+  },
+  {
+    vitamina: "Vitamina B1 (Tiamina)",
+    beneficios:
+      "Essencial para o funcionamento do sistema nervoso, músculos, desenvolvimento corporal e estímulo do apetite.",
+    fontes: [
+      "Gema de ovo",
+      "arroz integral",
+      "aveia",
+      "castanha do pará",
+      "fígado",
+      "cereais integrais",
+      "feijão",
+      "peixes",
+      "pão integral",
+    ],
+  },
+  {
+    vitamina: "Vitamina B2 (Riboflavina)",
+    beneficios:
+      "Atua no metabolismo de enzimas, células nervosas, equilíbrio da pele e saúde dos olhos.",
+    fontes: [
+      "Brócolis",
+      "abacate",
+      "amendoim",
+      "castanhas",
+      "levedo de cerveja",
+      "nozes",
+      "leite",
+      "carne",
+      "ervilhas",
+      "verduras",
+    ],
+  },
+  {
+    vitamina: "Vitamina B3 (Niacina)",
+    beneficios:
+      "Importante para o funcionamento do sistema digestório, equilíbrio do sistema nervoso e da pele.",
+    fontes: [
+      "Fígado",
+      "levedura de cerveja",
+      "carnes magras",
+      "ovos",
+      "leite",
+      "amendoim",
+      "castanha do pará",
+      "frutas secas",
+      "tomate",
+      "cenoura",
+    ],
+  },
+  {
+    vitamina: "Vitamina B5 (Ácido Pantotênico)",
+    beneficios:
+      "Participa da formação de anticorpos, metabolismo de proteínas, produção de hormônios suprarrenais e atuação contra o estresse.",
+    fontes: [
+      "Ervilha",
+      "feijão",
+      "cogumelo",
+      "ovos",
+      "gérmen de trigo",
+      "melado",
+      "salmão",
+    ],
+  },
+  {
+    vitamina: "Vitamina B6 (Piridoxina)",
+    beneficios:
+      "Auxilia na produção de células sanguíneas, metabolismo dos aminoácidos, funcionamento do sistema nervoso e saúde da pele.",
+    fontes: [
+      "Melado",
+      "levedo de cerveja",
+      "farelo de trigo",
+      "leite",
+      "arroz integral",
+      "aveia",
+      "cereais integrais",
+      "batata",
+      "melão",
+    ],
+  },
+  {
+    vitamina: "Vitamina B7 (Biotina)",
+    beneficios:
+      "Atua no processo de gliconeogênese, síntese de purinas, formação da pele e metabolismo de carboidratos e proteínas.",
+    fontes: [
+      "Fígado de galinha",
+      "fígado de boi",
+      "levedo de cerveja",
+      "gema de ovo crua",
+      "leite",
+      "nozes",
+      "gérmen de trigo",
+      "amendoim",
+      "aveia",
+    ],
+  },
+  {
+    vitamina: "Vitamina B9 (Ácido Fólico)",
+    beneficios:
+      "Importante para o tratamento de alguns tipos de anemias, saúde do feto durante a gravidez e controle da pressão sanguínea.",
+    fontes: [
+      "Verduras de folha verde",
+      "vísceras de animais",
+      "frutas secas",
+      "legumes",
+      "levedura de cerveja",
+      "grãos integrais",
+    ],
+  },
+  {
+    vitamina: "Vitamina B12 (Cobalamina)",
+    beneficios: "Essencial para o metabolismo de proteínas e combate à anemia.",
+    fontes: ["Carnes em geral", "ovos", "leite", "derivados"],
+  },
+];
